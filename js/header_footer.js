f2fe42001b9b4f9e74a48e85b24c7cec1f96f9c9
butterfly-nv <navyhim66@gmail.com>
2025-07-22 23:24:36 +0700

    Add files via upload

---
+++ js/header_footer.js
@@ -0,0 +1,36 @@
+let header=`<div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="images/logo.jpg" alt="Logo" />
+                </a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">Shop Now</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">Contact</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>`;
+
+document.getElementById("header").innerHTML = header;
+
+let footer=``
